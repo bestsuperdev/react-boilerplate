8ea9abf79515a2542d136248307e7ecfa9713e10
mingzepeng <mingzepeng@gmail.com>
2016-02-01 18:58:18 +0800

    add webpack.config.js;add clear fix

--- webpack.config.js
+++ webpack.config.js
@@ -13,20 +13,20 @@ module.exports = {
         main : path.join(__dirname,"./examples/scripts/main.js")   
     },
     output: {
-        path: path.join(__dirname,'hot'),
+        path: path.join(__dirname,'dist'),
         // publicPath: "/bundles/",
-        filename: "[name].[hash].bundle.js",
-        chunkFilename: "[id].[hash].chunk.js"
+        filename: "[name].bundle.js",
+        chunkFilename: "[id].chunk.js"
     },
     module: {
         loaders: [
-            { test : /\.css$/,  loader : 'style-loader!css-loader!postcss-loader' },
-            { test : /\.less$/, loader : 'style-loader!css-loader!postcss-loader!less-loader'},
-            { test : /\.jsx?$/, loader : 'react-hot!babel?presets[]=react,presets[]=es2015', exclude: /(node_modules|bower_components)/},
+            { test : /\.css$/,  loader : ExtractTextPlugin.extract('style-loader','css-loader!postcss-loader')},
+            { test : /\.less$/, loader : ExtractTextPlugin.extract('style-loader','css-loader!postcss-loader!less-loader')},
+            { test : /\.jsx?$/, loader : 'babel' , query : {presets : ['react','es2015'] , plugins : ['transform-es3-property-literals','transform-es3-member-expression-literals']} , exclude: /(node_modules|bower_components)/},
             // { test : /\.jsx?$/ , loader : 'babel-loader' , query:{ presets : ['es2015','react'] } , exclude: /(node_modules|bower_components)/},
             //如果不超过30000/1024kb,那么就直接采用dataUrl的形式,超过则返回链接,图片会复制到dist目录下
-            { test : /\.(png|jpg|jpeg|gif)$/, loader : "url-loader?limit=30000" },
-            { test : /\.(svg|ttf|eot|svg|woff(\(?2\)?)?)(\?[a-zA-Z_0-9.=&]*)?(#[a-zA-Z_0-9.=&]*)?$/, loader : "file-loader"}
+            { test: /\.(png|jpg|jpeg|gif)$/, loader: "url-loader?limit=30000" },
+            { test: /\.(svg|ttf|eot|svg|woff(\(?2\)?)?)(\?[a-zA-Z_0-9.=&]*)?(#[a-zA-Z_0-9.=&]*)?$/, loader : "file-loader"}
         ]
     },
 
@@ -41,15 +41,16 @@ module.exports = {
         new webpack.DefinePlugin({
             __DEBUG__: true
         }),
-        new webpack.optimize.CommonsChunkPlugin("commons", "[name].[hash].bundle.js"),
-        new webpack.HotModuleReplacementPlugin(),
+        new ExtractTextPlugin("[name].bundle.css",{allChunks: true}),
+        new webpack.optimize.CommonsChunkPlugin("commons", "[name].bundle.js"),
+        // new webpack.HotModuleReplacementPlugin(),
         new HtmlWebpackPlugin({
             template : 'examples/index.html',
             inject: true
             // filename: '../index.html',
         })
-    ],
-    debug : true,
-    devtool : 'cheap-module-eval-source-map'
+    ]
+    // debug : true,
+    // devtool : 'cheap-module-eval-source-map'
     //devServer 配置在webpack.dev.server.js 中
 };
